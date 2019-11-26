547d091775e0fbd78ff5278c8c4cf45043bf3dcc
Momchil Radulov <momchil_radulov@yahoo.com>
2019-11-26 11:44:30 +0200

    Update hug1.py

--- hug1.py
+++ hug1.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import hug, dataset
 # gunicorn3 hug1:__hug_wsgi__ # how to start
+# pkill gunicorn # how to stop
 # ssh -L 8000:localhost:8000 ubuntu@host # how to connect and test from external computer
 # http://localhost:8000/ # how to test with browser
 db = dataset.connect('sqlite:////root/git/db/users.db') # path to sqlite3 database file
