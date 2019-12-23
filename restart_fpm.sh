ac6f8314c8476bf92bc83082298c381e8bff6b64
Momchil Radulov <momchil_radulov@yahoo.com>
2019-12-23 11:39:53 +0200

    Create restart_fpm.sh

---
+++ restart_fpm.sh
@@ -0,0 +1,9 @@
+lockResult=$(lsof /root/git/db/users.db | wc -l 2>&1)
+if [ "$lockResult" -gt 2 ]; then
+    currentDate=`date`
+    service php7.0-fpm restart
+    echo "$currentDate " >> /root/git/db/restart_fpm.log;
+    echo "Restart php7.0-fpm" >> /root/git/db/restart_fpm.log;
+else
+    lockResultOK="OK"
+fi
