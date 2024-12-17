47b0553860bdf374a8447a651d81a9c0ea39080d
Momchil Radulov <momchil_radulov@yahoo.com>
2024-12-18 00:48:32 +0200

    Create socat.sh

---
+++ socat.sh
@@ -0,0 +1,5 @@
+# http traffic
+socat -v tcp-listen:80,reuseaddr tcp:some_domain.com:80
+in /etc/hosts insert: "127.0.0.1 some_domain.com"
+# smtp traffic
+socat -v tcp-listen:25,reuseaddr tcp:your-smtp-server.foo:25
