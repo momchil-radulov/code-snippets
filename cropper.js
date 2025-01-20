75aaac2ca63269712b7981cefdbe6ec33ce7995e
Momchil Radulov <momchil_radulov@yahoo.com>
2025-01-20 12:16:10 +0200

    Create cropper.js

---
+++ cropper.js
@@ -0,0 +1,25 @@
+// Зареждане на изображението
+imageInput.addEventListener("change", (event) => {
+    const file = event.target.files[0];
+    if (file) {
+        $('#preview').show();
+        // Създаване на URL от файла
+        const fileURL = URL.createObjectURL(file);
+        
+        // Актуализиране на източника на изображението
+        preview.src = fileURL;
+
+        // Ако вече има cropper, унищожаваме го
+        if (cropper) cropper.destroy();
+
+        // Създаване на нов Cropper
+        cropper = new Cropper(preview, {
+            viewMode: 2,
+        });
+
+        // Освобождаване на URL-а, когато вече не е необходим
+        preview.onload = () => {
+            URL.revokeObjectURL(fileURL);
+        };
+    }
+});
